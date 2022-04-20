ec6c9d36405576f948dcfd93da745b3559802d02
Andy <andyludeveloper@gmail.com>
2022-04-20 23:55:18 +0800

    Update environment parameters

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,5 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.17"
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    languageVersion = "1.17"
+    kotlinOptions.jvmTarget = "17"
 }
