db41a1385b15682b5442fe9d30c56cde595703b8
Andy Lu <alu@futuredial.com>
2022-04-22 15:21:06 +0800

    [Listing 6.2] Using Option to return a value from a Map

---
+++ src/main/kotlin/com/andyludeveloper/the_joy_of_kotlin/ch6/Toon.kt
@@ -0,0 +1,32 @@
+package com.andyludeveloper.the_joy_of_kotlin.ch6
+
+data class Toon(
+    val firstName: String,
+    val lastName: String,
+    val email: Option<String> = Option()
+) {
+    companion object {
+        operator fun invoke(
+            firstName: String,
+            lastName: String,
+            email: String? = null
+        ) =
+            Toon(firstName, lastName, Option(email))
+    }
+}
+
+fun <K, V> Map<K, V>.getOption(key: K) = Option(this[key])
+
+fun main() {
+    val toons: Map<String, Toon> = mapOf(
+        "Mickey" to Toon("Mickey", "Mouse", "mickey@disney.com"),
+        "Minnie" to Toon("Minnie", "Mouse"),
+        "Donald" to Toon("Donald", "Duck", "donald@disney.com")
+    )
+    val mickey = toons.getOption("Mickey").flatMap { it.email }
+    val minnie = toons.getOption("Minnie").flatMap { it.email }
+    val goofy = toons.getOption("Goofy").flatMap { it.email }
+    println(mickey.getOrElse { " No Data" })
+    println(minnie.getOrElse { " No Data" })
+    println(goofy.getOrElse { " No Data" })
+}
